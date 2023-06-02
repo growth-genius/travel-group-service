638e30ca5f4653b34481df6b95de19b8feb1f2b2
yeji.jo <choyeji1591@gmail.com>
2023-06-02 22:56:47 +0900

    feat : first setting

---
+++ src/main/java/com/gg/tgather/travelgroupservice/TravelGroupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gg.tgather.travelgroupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGroupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelGroupServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gg/tgather/travelgroupservice/TravelGroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gg.tgather.travelgroupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelGroupServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
