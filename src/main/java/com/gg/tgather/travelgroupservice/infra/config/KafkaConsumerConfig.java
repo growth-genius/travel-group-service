f6ab05473190bf08302c30069113e12b635dfa25
cyeji <choyeji1591@gmail.com>
2023-06-14 12:52:42 +0900

    fix : kafkaConfig groupIdConfig 수정

--- src/main/java/com/gg/tgather/travelgroupservice/infra/config/KafkaConsumerConfig.java
+++ src/main/java/com/gg/tgather/travelgroupservice/infra/config/KafkaConsumerConfig.java
@@ -24,7 +24,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigProperties.getBootstrapServersConfig());
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfigProperties.getGroupIdConfig());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(properties);
