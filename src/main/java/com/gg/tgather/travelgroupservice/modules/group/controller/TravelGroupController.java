d14f57767450383013ccfb6c2a6e28ba7b59c485
seunggulee1007 <leesg107@naver.com>
2023-06-27 18:20:06 +0900

    feat: 여행 그룹 오픈 api 추가

--- src/main/java/com/gg/tgather/travelgroupservice/modules/group/controller/TravelGroupController.java
+++ src/main/java/com/gg/tgather/travelgroupservice/modules/group/controller/TravelGroupController.java
@@ -3,21 +3,16 @@ package com.gg.tgather.travelgroupservice.modules.group.controller;
 import static com.gg.tgather.commonservice.utils.ApiUtil.success;
 
 import com.gg.tgather.commonservice.annotation.RestBaseAnnotation;
-import com.gg.tgather.commonservice.enums.TravelTheme;
 import com.gg.tgather.commonservice.security.JwtAuthentication;
 import com.gg.tgather.commonservice.utils.ApiUtil.ApiResult;
 import com.gg.tgather.travelgroupservice.modules.group.dto.TravelGroupDto;
 import com.gg.tgather.travelgroupservice.modules.group.dto.TravelGroupRegisterInitDto;
-import com.gg.tgather.travelgroupservice.modules.group.dto.TravelGroupWithPageable;
 import com.gg.tgather.travelgroupservice.modules.group.form.TravelGroupModifyForm;
 import com.gg.tgather.travelgroupservice.modules.group.form.TravelGroupSaveForm;
 import com.gg.tgather.travelgroupservice.modules.group.service.TravelGroupService;
 import jakarta.validation.Valid;
 import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +20,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 여행그룹 CRUD Restful API
@@ -35,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 2023.06.04
  */
 @RestBaseAnnotation
-@RequestMapping("/group")
 @RequiredArgsConstructor
 public class TravelGroupController {
 
@@ -58,7 +50,7 @@ public class TravelGroupController {
      * @param authentication      계정 인증
      * @return TravelGroupDto 여행그룹 생성 결과
      */
-    @PostMapping
+    @PostMapping("/group")
     public ApiResult<TravelGroupDto> createTravelGroup(@RequestBody @Valid TravelGroupSaveForm travelGroupSaveForm,
         @AuthenticationPrincipal JwtAuthentication authentication) {
         return success(travelGroupService.createTravelGroup(travelGroupSaveForm, authentication));
@@ -77,26 +69,6 @@ public class TravelGroupController {
         return success(travelGroupService.modifyTravelGroup(travelGroupId, travelGroupModifyForm, authentication));
     }
 
-    /**
-     * 여행그룹 전체조회 API
-     *
-     * @return 여행그룹 전체 결과값
-     */
-    @GetMapping("/all")
-    public ApiResult<TravelGroupWithPageable> findAllTravelGroups(@PageableDefault(sort = "id") Pageable pageable) {
-        return success(travelGroupService.findAllTravelGroupsWithPageable(pageable));
-    }
-
-    /**
-     * 여행테마로 그룹 찾기 API
-     *
-     * @param travelThemes 여행 테마
-     * @return List<TravelGroupDto> 조건에 부합한 여행그룹들
-     */
-    @GetMapping
-    public ApiResult<List<TravelGroupDto>> findTravelGroupByTravelThemes(@RequestParam Set<TravelTheme> travelThemes) {
-        return success(travelGroupService.findTravelGroupByTheme(travelThemes));
-    }
 
     /**
      * 로그인 사용자의 여행그룹 조회
---
+++ src/main/java/com/gg/tgather/travelgroupservice/modules/group/controller/TravelGroupOpenController.java
@@ -0,0 +1,48 @@
+package com.gg.tgather.travelgroupservice.modules.group.controller;
+
+import static com.gg.tgather.commonservice.utils.ApiUtil.success;
+
+import com.gg.tgather.commonservice.annotation.RestBaseAnnotation;
+import com.gg.tgather.commonservice.enums.TravelTheme;
+import com.gg.tgather.commonservice.utils.ApiUtil.ApiResult;
+import com.gg.tgather.travelgroupservice.modules.group.dto.TravelGroupDto;
+import com.gg.tgather.travelgroupservice.modules.group.dto.TravelGroupWithPageable;
+import com.gg.tgather.travelgroupservice.modules.group.service.TravelGroupService;
+import java.util.List;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestBaseAnnotation
+@RequestMapping("/open")
+@RequiredArgsConstructor
+public class TravelGroupOpenController {
+
+    private final TravelGroupService travelGroupService;
+
+    /**
+     * 여행테마로 그룹 찾기 API
+     *
+     * @param travelThemes 여행 테마
+     * @return List<TravelGroupDto> 조건에 부합한 여행그룹들
+     */
+    @GetMapping("/group")
+    public ApiResult<List<TravelGroupDto>> findTravelGroupByTravelThemes(@RequestParam Set<TravelTheme> travelThemes) {
+        return success(travelGroupService.findTravelGroupByTheme(travelThemes));
+    }
+
+    /**
+     * 여행그룹 전체조회 API
+     *
+     * @return 여행그룹 전체 결과값
+     */
+    @GetMapping("/all")
+    public ApiResult<TravelGroupWithPageable> findAllTravelGroups(@PageableDefault(sort = "id") Pageable pageable) {
+        return success(travelGroupService.findAllTravelGroupsWithPageable(pageable));
+    }
+
+}
